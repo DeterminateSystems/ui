28de2148ea41960288c9496f4cc2aab7447f7c43
gustavderdrache <alex.ford@determinate.systems>
2025-06-05 11:25:27 -0400

    new hook: `useHighlight`

---
+++ src/hooks/useHighlight.ts
@@ -0,0 +1,51 @@
+import { createHighlighterCoreSync } from "@shikijs/core";
+import { createJavaScriptRegexEngine } from "@shikijs/engine-javascript";
+import langShell from "@shikijs/langs/shellscript";
+import langYaml from "@shikijs/langs/yaml";
+import themeGitHubDark from "@shikijs/themes/github-dark";
+import themeGitHubLight from "@shikijs/themes/github-light";
+import { useMemo } from "react";
+
+/**
+ * Languages understood by the UI system's highlighter. The `text` option prevents highlighting.
+ */
+export type HighlightLanguage = "shell" | "yaml" | "text";
+
+// Lazily instantiate the Shiki renderer to avoid paying (some) startp costs
+let shiki: ReturnType<typeof createHighlighterCoreSync>;
+function getShiki() {
+  return (shiki ??= createHighlighterCoreSync({
+    themes: [themeGitHubLight, themeGitHubDark],
+    langs: [langShell, langYaml],
+    engine: createJavaScriptRegexEngine(),
+  }));
+}
+
+/**
+ * Hook to highlight code. The rendered HTML is memoized to avoid excessive re-computing of the syntax highlighting for various documents.
+ *
+ * @param language The language to highlight. `'text'` turns highlighting off.
+ * @param code The code to highlight.
+ *
+ * @returns Safe-to-inject HTML representing highlighting.
+ */
+function useHighlight(
+  language: HighlightLanguage,
+  code: string,
+): string {
+  return useMemo(() => {
+    const html = getShiki().codeToHtml(code, {
+      lang: language,
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      defaultColor: false,
+      structure: "inline",
+    });
+
+    return html;
+  }, [language, code]);
+}
+
+export default useHighlight;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { type LinkProps, default as Link } from "./Link";
+export { type HighlightLanguage, default as useHighlight } from "./hooks/useHighlight";
