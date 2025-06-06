5c15fce8b0cf8327adcc732ffb955bb884d4b7f9
gustavderdrache <alex.ford@determinate.systems>
2025-06-06 16:06:58 -0400

    Turn on StrictMode to catch more errors

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,22 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     {
-      "name": "@storybook/addon-essentials",
-      "options": {
-        "docs": false
-      }
+      name: "@storybook/addon-essentials",
+      options: {
+        docs: false,
+      },
     },
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@whitespace/storybook-addon-html",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      strictMode: true,
+    },
+  },
 };
 export default config;
