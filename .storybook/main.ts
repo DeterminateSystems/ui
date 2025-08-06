f4e653fb8c31f107d87582d3a282aa0bc0599ca2
Graham Christensen <graham@grahamc.com>
2025-08-06 09:46:13 -0400

    Drop storybook-addon-html since it is not yet compatible with 9.x

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,8 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-a11y",
-    "@whitespace/storybook-addon-html",
     "@storybook/addon-docs",
+    // "@whitespace/storybook-addon-html", // 7.0.0 (current as of 2025-08-06) is incompatible with storyboox 9.x
   ],
   framework: {
     name: "@storybook/react-vite",
