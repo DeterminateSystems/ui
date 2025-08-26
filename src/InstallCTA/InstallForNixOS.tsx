f97eb8f39bf349c8474286cdc6134ba752fef2de
Graham Christensen <graham@grahamc.com>
2025-08-26 12:55:08 -0400

    Tidy up

--- src/InstallCTA/InstallForNixOS.tsx
+++ src/InstallCTA/InstallForNixOS.tsx
@@ -15,18 +15,21 @@ nixosConfigurations.workstation = nixpkgs.lib.nixosSystem {
 const InstallForNixOS: FC = () => {
   return (
     <>
-      <p></p>
+      <p>Add Determinate as an input to your flake:</p>
       <CodeBlock
-        kind="snippet"
         language={"nix"}
-        title={"Add Determinate as an input to your flake:"}
+        title={"flake.nix"}
         code={`inputs.determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";`}
+        kind={"file"}
+        allowDownload={false}
       />
+      <p>Then, include the Determinate NixOS module:</p>
       <CodeBlock
-        kind="snippet"
         language={"nix"}
-        title={"Then include the Determinate NixOS module:"}
+        title={"flake.nix"}
         code={code}
+        kind={"file"}
+        allowDownload={false}
       />
 
       <p>
