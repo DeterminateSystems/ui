bba4298e21719c53d077c69d23c4a093a8360b53
Graham Christensen <graham@grahamc.com>
2025-12-09 14:48:32 -0500

    CopyButton: extract the copy logic out into a useCopy

--- src/CopyButton/index.tsx
+++ src/CopyButton/index.tsx
@@ -1,17 +1,11 @@
-import {
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-  type FC,
-  type SyntheticEvent,
-} from "react";
+import { type FC } from "react";
 import {
   ClipboardDocumentIcon,
   ClipboardDocumentCheckIcon,
 } from "@heroicons/react/24/outline";
 
 import "./index.scss";
+import useCopy from "../hooks/useCopy";
 
 export interface CopyButtonProps {
   /**
@@ -20,53 +14,23 @@ export interface CopyButtonProps {
   data: string;
 }
 
-const delay = 1_000;
-
 /**
  * A button to copy string data to the user's clipboard. This is most typically used for snippets of code.
  *
  * A small amount of visual feedback is given when the data is copied successfully.
  */
 const CopyButton: FC<CopyButtonProps> = ({ data }) => {
-  const [copied, setCopied] = useState(false);
-  const ref = useRef<number | null>(null);
+  const [handleCopy, copied] = useCopy(data);
 
   const Icon = copied ? ClipboardDocumentCheckIcon : ClipboardDocumentIcon;
-
-  const handleClick = useCallback(
-    (event: SyntheticEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-
-      navigator.clipboard
-        .writeText(data)
-        .then(() => {
-          setCopied(true);
-
-          const id = setTimeout(() => {
-            setCopied(false);
-            ref.current = null;
-          }, delay);
-
-          ref.current = id;
-        })
-        .catch((error) => {
-          console.error("Couldn't write to clipboard", error);
-        });
-    },
-    [data],
-  );
-
-  useEffect(() => {
-    const id = ref.current;
-    if (id !== null) {
-      clearTimeout(id);
-    }
-  }, [ref]);
 
   return (
     <button
       className="copy-button"
-      onClick={handleClick}
+      onClick={(event) => {
+        event.preventDefault();
+        handleCopy();
+      }}
       aria-label="Copy text"
     >
       <Icon />
